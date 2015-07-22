9cb1ed09a98195e7d41dc356096c8f02315bbdaf
Tsgzj <wxsun1991@gmail.com>
2015-07-22 16:38:49 -0700

    Added README

--- TripadvisorReview/spiders/Tadv_spider.py
+++ TripadvisorReview/spiders/Tadv_spider.py
@@ -8,6 +8,7 @@ MAX_REVIEWS_PAGES = 10
 
 from HTMLParser import HTMLParser
 
+# Strip html string
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
@@ -41,18 +42,17 @@ class TadvSpider(scrapy.Spider):
       self.start_urls = kwargs.get('start_urls').split(',')
 
     def parse(self, response):
-        if (self.counter_page_review < MAX_REVIEWS_PAGES or MAX_REVIEWS_PAGES <
-        0):
+        if (self.counter_page_review < MAX_REVIEWS_PAGES or MAX_REVIEWS_PAGES <= 0):
             self.counter_page_review += 1
 
             for sel in response.xpath("//div[@id='REVIEWS']"):
-                raw_title = sel.xpath("//div[@class='quote isNew']/a/span[@class='noQuotes']").extract()
+                #raw_title = sel.xpath("//div[@class='quote isNew']/a/span[@class='noQuotes']").extract()
                 raw_entry = sel.xpath("//div[@class='wrap']/div[@class='entry']").extract()
-                stripped_title = strip_all(raw_title)
+                #stripped_title = strip_all(raw_title)
                 stripped_entry = strip_all(raw_entry)
 
                 tripadvisor_item = TripadvisorreviewItem()
-                tripadvisor_item['title'] = stripped_title
+                #tripadvisor_item['title'] = stripped_title
                 tripadvisor_item['entry'] = stripped_entry
 
                 self.tripadvisor_items.append(tripadvisor_item)
@@ -66,37 +66,4 @@ class TadvSpider(scrapy.Spider):
                     yield self.tripadvisor_items
 
             else:
-                yield self.tripadvisor_items
-
-
-           # yield scrapy.Request(self.base_url + next_page_url, meta={'tripadvisor_item': tripadvisor_item, 'counter_page_review': 0}, callback=self.parse_follow_next_page)
-
-    # def parse_follow_next_page(self, response):
-    #     tripadvisor_item = response.meta['tripadvisor_item']
-    #     counter_page_review = response.meta['counter_page_review']
-    #     sel = scrapy.Selector(response)
-
-    #     if counter_page_review < MAX_REVIEWS_PAGES:
-    #         counter_page_review = counter_page_review + 1
-
-    #         for i in sel.xpath("//div[@id='REVIEWS']"):
-    #             raw_title = i.xpath("//div[@class='quote isNew']/a/span[@class='noQuotes']").extract()
-    #             raw_entry = i.xpath("//div[@class='wrap']/div[@class='entry']").extract()
-    #             stripped_title = strip_all(raw_title)
-    #             stripped_entry = strip_all(raw_entry)
-
-    #             tripadvisor_item['title'] = stripped_title
-    #             tripadvisor_item['entry'] = stripped_entry
-
-    #         next_page_url = strip_tags(sel.xpath("//div[@class='deckTools btm']//a[2]/@href").extract()[0])
-
-    #         if next_page_url and len(next_page_url) > 0:
-    #             yield scrapy.Request(self.base_url + next_page_url,
-    #             meta={'tripadvisor_item': tripadvisor_item,
-    #                   'counter_page_review': counter_page_review},
-    #             callback=self.parse_follow_next_page)
-    #         else:
-    #             yield tripadvisor_item
-
-    #     else:
-    #         yield tripadvisor_item
+                yield self.tripadvisor_tems
